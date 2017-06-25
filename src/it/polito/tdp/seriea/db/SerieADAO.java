bcbeb3ba49801f1ce5a8b5bb76b55ea9cca7f35b
Tiberio <Tiberio@192.168.1.135>
2017-06-25 18:34:35 +0200

    finale

--- src/it/polito/tdp/seriea/db/SerieADAO.java
+++ src/it/polito/tdp/seriea/db/SerieADAO.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import it.polito.tdp.seriea.model.BigMatch;
 import it.polito.tdp.seriea.model.Match;
+import it.polito.tdp.seriea.model.MatchIdMap;
 import it.polito.tdp.seriea.model.Season;
 import it.polito.tdp.seriea.model.Team;
 import it.polito.tdp.seriea.model.TeamIdMap;
@@ -329,5 +330,169 @@ public class SerieADAO {
 			return null ;
 		}
 	}
-
+	
+	public ArrayList<Match> getAllMacthTraDueTeam(Team t1 , Team t2, TeamIdMap mappaTeam,MatchIdMap mappaMatch) {
+		String sql = "select matches.match_id,matches.Season,matches.`Div`,matches.Date,matches.HomeTeam,matches.AwayTeam,matches.FTHG,matches.FTAG,matches.FTR " + 
+				"from matches  " + 
+				"where (matches.HomeTeam=? and matches.AwayTeam=?) or (matches.AwayTeam=? and matches.HomeTeam=?)";
+		
+		ArrayList<Match> result = new ArrayList<Match>() ;
+		
+		Connection conn = DBConnect.getConnection() ;
+		
+		try {
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			
+			st.setString(1, t1.getTeam());
+			
+			st.setString(2,t2.getTeam() );
+			
+			st.setString(3, t1.getTeam());
+			
+			st.setString(4,t2.getTeam() );
+			
+			ResultSet res = st.executeQuery() ;
+			
+			
+			while(res.next()) {
+				Match m=mappaMatch.get(res.getInt("matches.match_id"));
+				if(m==null){
+							Team casa= mappaTeam.get(res.getString("matches.HomeTeam"));
+							Team ospite= mappaTeam.get(res.getString("matches.AwayTeam"));
+							
+							if(casa==null||ospite==null){
+								throw new IllegalArgumentException("ERRORE! Il team non è presente nella mappa!");
+							}
+							
+							m=new Match(
+									res.getInt("matches.match_id"),
+									null,
+								//	s,  nel caso trasformi season in match in un intero
+									res.getString("matches.Div"),
+									res.getDate("matches.Date").toLocalDate(),
+									casa,
+									ospite,
+									res.getInt("matches.FTHG"),
+									res.getInt("matches.FTAG"),
+									res.getString("matches.FTR"));
+							mappaMatch.put(m);
+						}
+				result.add(m);
+			}
+			conn.close();
+			return result ;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null ;
+		}
+	}
+	public ArrayList<Match> getAllMacthDiUnTeam(Team t1 ,TeamIdMap mappaTeam) {
+		String sql = "select matches.match_id,matches.Season,matches.`Div`,matches.Date,matches.HomeTeam,matches.AwayTeam,matches.FTHG,matches.FTAG,matches.FTR   " + 
+				"from matches   " + 
+				"where matches.HomeTeam=? or matches.AwayTeam=?";
+		
+		ArrayList<Match> result = new ArrayList<Match>() ;
+		
+		Connection conn = DBConnect.getConnection() ;
+		
+		try {
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			
+			st.setString(1, t1.getTeam());
+			
+			st.setString(2, t1.getTeam());
+			
+			ResultSet res = st.executeQuery() ;
+			
+			
+			while(res.next()) {
+				Team casa= mappaTeam.get(res.getString("matches.HomeTeam"));
+				Team ospite= mappaTeam.get(res.getString("matches.AwayTeam"));
+				
+				if(casa==null||ospite==null){
+					throw new IllegalArgumentException("ERRORE! Il team non è presente nella mappa!");
+				}
+				
+				Match m=new Match(
+						res.getInt("matches.match_id"),
+						null,
+					//	s,  nel caso trasformi season in match in un intero
+						res.getString("matches.Div"),
+						res.getDate("matches.Date").toLocalDate(),
+						casa,
+						ospite,
+						res.getInt("matches.FTHG"),
+						res.getInt("matches.FTAG"),
+						res.getString("matches.FTR"));
+				
+				result.add(m);
+			}
+			
+			conn.close();
+			return result ;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null ;
+		}
+	}
+	
+	public ArrayList<Match> getAllMacthDiUnTeam(Team t1 , TeamIdMap mappaTeam,MatchIdMap mappaMatch) {
+		String sql = "select matches.match_id,matches.Season,matches.`Div`,matches.Date,matches.HomeTeam,matches.AwayTeam,matches.FTHG,matches.FTAG,matches.FTR  " + 
+				"from matches  " + 
+				"where matches.HomeTeam=? or matches.AwayTeam=?";
+		
+		ArrayList<Match> result = new ArrayList<Match>() ;
+		
+		Connection conn = DBConnect.getConnection() ;
+		
+		try {
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			
+			st.setString(1, t1.getTeam());
+			
+			st.setString(2, t1.getTeam());
+			
+			ResultSet res = st.executeQuery() ;
+			
+			
+			while(res.next()) {
+				Match m=mappaMatch.get(res.getInt("matches.match_id"));
+				if(m==null){
+							Team casa= mappaTeam.get(res.getString("matches.HomeTeam"));
+							Team ospite= mappaTeam.get(res.getString("matches.AwayTeam"));
+							
+							if(casa==null||ospite==null){
+								throw new IllegalArgumentException("ERRORE! Il team non è presente nella mappa!");
+							}
+							
+							m=new Match(
+									res.getInt("matches.match_id"),
+									null,
+								//	s,  nel caso trasformi season in match in un intero
+									res.getString("matches.Div"),
+									res.getDate("matches.Date").toLocalDate(),
+									casa,
+									ospite,
+									res.getInt("matches.FTHG"),
+									res.getInt("matches.FTAG"),
+									res.getString("matches.FTR"));
+							
+							mappaMatch.put(m);
+						}
+				result.add(m);
+			}
+			conn.close();
+			return result ;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null ;
+		}
+	}
+	//TUTTI I MACH DOVE C'E' STATO IL MAGGIOR NUMERO DI GOL FUOI CASA
+	//select matches.match_id,matches.Season,matches.`Div`,matches.Date,matches.HomeTeam,matches.AwayTeam,matches.FTHG,matches.FTAG,matches.FTR  
+	//from matches 
+	//where  matches.FTAG=(select max(matches.FTAG) from matches)
 }
--- src/it/polito/tdp/seriea/db/TestSerieADAO.java
+++ src/it/polito/tdp/seriea/db/TestSerieADAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import it.polito.tdp.seriea.model.BigMatch;
 import it.polito.tdp.seriea.model.Match;
+import it.polito.tdp.seriea.model.MatchIdMap;
 import it.polito.tdp.seriea.model.Season;
 import it.polito.tdp.seriea.model.Team;
 import it.polito.tdp.seriea.model.TeamIdMap;
@@ -15,25 +16,37 @@ public class TestSerieADAO {
 		SerieADAO dao = new SerieADAO() ;
 		
 		List<Season> seasons = dao.listSeasons() ;
-		System.out.println(seasons);
+		System.out.println("a"+seasons);
 		
 		TeamIdMap mappaTeam =new TeamIdMap();
 		
 		List<Team> teams = dao.listTeams(mappaTeam) ;
-		System.out.println(teams);
+		System.out.println("b"+teams);
 
 		
 		
 		dao.getAllTeamsInThisSeason(seasons.get(0), mappaTeam);
 		
 		List<BigMatch> bigMatches = dao.getAllBigMacthInThisSeason(seasons.get(0), mappaTeam);
-		System.out.println(bigMatches);
+		System.out.println("c"+bigMatches);
 		
 		List<Match> listaa=dao.getAllMacthInThisPeriod(LocalDate.of(2015, 8, 29),LocalDate.of(2015, 12, 29), mappaTeam);
-		System.out.println(listaa);
+		System.out.println("d"+listaa);
 		
 		List<Match> listaaa=dao.getAllMacthTraDueTeam(new Team("Inter"),new Team("Lecce"), mappaTeam);
-		System.out.println(listaaa);
+		System.out.println("e"+listaaa);
+		
+		MatchIdMap mappaMatch =new MatchIdMap();
+		
+		List<Match> listaaaa=dao.getAllMacthTraDueTeam(new Team("Inter"),new Team("Lecce"), mappaTeam,mappaMatch);
+		System.out.println("f"+listaaaa);
+		
+		List<Match> listaaaaa=dao.getAllMacthDiUnTeam(new Team("Inter"), mappaTeam);
+		System.out.println("g"+listaaaaa);
+		
+		List<Match> lista1=dao.getAllMacthDiUnTeam(new Team("Inter"), mappaTeam,mappaMatch);
+		System.out.println("h"+lista1);
+		
 		
 	}
 
